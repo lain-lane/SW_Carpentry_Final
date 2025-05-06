48672cd6b7f8d6b003614c00f23ad31c7e825d4d
lain-lane <eorr2@jh.edu>
2025-05-06 13:57:13 -0400

    Add files via upload

--- example_main.py
+++ example_main.py
@@ -3,7 +3,7 @@ from Enemies import Snow_Golem, Dragon, Goblin, Snow_Sprite, Magma_Cube
 from Player import Player
 from Game import Tutorial, Load, Save, Fight
 
-''' This script is a simple example of how the main code is run. The player Joe can be initiated with the Load Function
+''' This script is a simple example of how the main code is run. The player can be initiated with the Load Function
 or by directly defining the hp, max_hp, and xp. The enemy is initiated as one of the imported monsters. Calling Fight
 starts the game which can then be saved using the Save function. Calling Tutorial() prints the rules.'''
 
